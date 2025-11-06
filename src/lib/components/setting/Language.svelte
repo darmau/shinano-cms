c16c59ae08269a385ecebd30edb0e8fd933fa001
李大毛 <github@darmau.co>
2025-11-06 21:52:41 +0900

    style: update border color in Language component for improved visual consistency

--- src/lib/components/setting/Language.svelte
+++ src/lib/components/setting/Language.svelte
@@ -113,7 +113,7 @@
 			</div>
 		{:else}
 			{#each languages as language}
-				<div class = "border-b flex justify-between py-4">
+				<div class = "border-b border-gray-200 flex justify-between py-4">
 					<div>
 						<h3 class = "font-medium flex items-center gap-2">
 							{language.locale}
