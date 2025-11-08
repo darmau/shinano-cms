1138da3914b0fab263acdd031deb8de76b5b23e4
李大毛 <github@darmau.co>
2025-11-08 13:45:04 +0900

    chore: update configuration files for Cloudflare integration and SvelteKit enhancements

--- svelte.config.js
+++ svelte.config.js
@@ -5,17 +5,25 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	compilerOptions: {
-		runes: false
+		runes: undefined,
+		experimental: {
+			async: true
+		}
 	},
-	preprocess: [vitePreprocess(), preprocess()],
+	preprocess: [vitePreprocess()],
 	kit: {
+		experimental: {
+			remoteFunctions: true
+		},
 		alias: {
 			'$assets': './src/assets',
 			'$components': './src/lib/components',
 			'$svelte-kit': './.svelte-kit',
 			'$types': './src/types',
 		},
-		adapter: adapter()
+		adapter: adapter({
+			config: 'wrangler.jsonc'
+		})
 	}
 };
 
